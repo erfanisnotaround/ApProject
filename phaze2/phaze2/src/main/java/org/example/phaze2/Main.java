e4cc071a471939d26e55a84be6e14e05e42f5eaa
Your Name <you@example.com>
2025-06-14 17:01:42 +0330

    can we Start??

---
+++ phaze2/phaze2/src/main/java/org/example/phaze2/Main.java
@@ -0,0 +1,16 @@
+package org.example.phaze2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Hello World!");
+    }
+}
